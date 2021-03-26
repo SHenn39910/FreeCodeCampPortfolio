36c3d7684c03fe2223c4e89db0b4bad80ee724da
Sonja Hennessy <sonjahennessy15@gmail.com>
2021-03-26 13:11:10 -0400

    added navtoggle

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
 
-navToggle.addEventListener('click', () =>)
+navToggle.addEventListener('click', () =>{
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove()
+    })
+})
