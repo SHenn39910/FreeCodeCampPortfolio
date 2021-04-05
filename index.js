a47a6ff0e8f195e387305801792a488e27d9ed09
Sonja Hennessy <sonjahennessy15@gmail.com>
2021-04-04 22:03:33 -0400

    added styling for about me

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
-navToggle.addEventListener('click', () =>{
+navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
 
@@ -9,4 +9,4 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     })
-});
+})
